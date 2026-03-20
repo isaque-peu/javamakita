989f0ce99da2297cf3b9ea11a99bd9759ef3e66d
isaque-peu <isaquefreitas356@gmail.com>
2026-03-20 17:35:26 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+//exercicio 10//
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -30,3 +31,75 @@ public class Main {
         
     }
 }
+
+//Exercicio 11//
+public class main {
+    public static void main(String[] args);
+        int soma = 0;
+    for(int ram = 1; ram <= 100; ram++){
+        if(ram % 2 == 0){
+        soma += ram;
+        }
+     }
+        System.out.println("Soma dos pares 1 a 100: " + soma);
+
+    }
+}
+
+//exercicio 12//
+public class main {
+    public static void main(String[] args){
+    int positivo = 0;
+    int negativo = 0;
+    
+
+     for(int ram = -10;ram <= 10; ram++){
+       if(ram > 0){
+           positivo++;
+       }else if (ram < 0){
+           negativo++;
+       }
+        System.out.println("positivo" + positivo);
+        System.out.println("negativo" + negativo);
+      }
+    }
+}
+
+//exercicio 13//
+public class main {
+    public static void main(String[] args){
+   
+
+    for(int ram = 1; ram <= 5; ram++){
+      System.out.println("Tabuada do" + ram);
+      
+     for(int s = 1; s <= 10; s++){
+      int resultado =  ram * s;
+         System.out.println( ram + "x" +s+ "=" + resultado);
+     
+      
+      }}
+    }}
+
+//exercicio 14//
+public class main {
+    public static void main(String[] args){
+   
+     double n1 = 80;
+     double n2 = 60;
+     double n3 = 70;
+     double n4 = 50;
+     double n5 = 90;
+     
+double media = (n1 + n2 + n3 + n4 + n5)/5;
+
+System.out.println("Media: " + media);
+
+if(media >= 70){
+    System.out.println("Aprovado");
+}else if (media >= 50){ 
+    System.out.println("Recuperação");
+}else{
+    System.out.println("Reprovado");
+} 
+}}
